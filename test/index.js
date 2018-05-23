5e73a8c3b10d1e9599a724b968346a1d3c3e5954
Kevin Grandon <kevingrandon@yahoo.com>
2018-05-23 10:50:13 -0700

    Add flow annotations

--- test/index.js
+++ test/index.js
@@ -1,3 +1,11 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
 const test = require('tape');
 const fs = require('fs');
 const {join} = require('path');
